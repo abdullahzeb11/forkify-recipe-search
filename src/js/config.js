3c52689ed7758f4979a8db636490e08858fee8ce
DESKTOP-OJRHMUL\abdul <abdullahzeb.00@gmail.com>
2024-05-18 19:16:58 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'cfe1e01b-f605-4ec2-984c-eb60fb4215a6';
+export const MODAL_CLOSE_SEC = 2.5;
