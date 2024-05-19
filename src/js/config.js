6ffbb69b03c5b48e5408b90aeb8ad284b354de51
DESKTOP-OJRHMUL\abdul <abdullahzeb.00@gmail.com>
2024-05-19 10:07:42 +0300

    key change

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = 'cfe1e01b-f605-4ec2-984c-eb60fb4215a6';
+export const KEY = '1365f6f5-025d-40ae-99c4-667547a5aa18';
 export const MODAL_CLOSE_SEC = 2.5;
